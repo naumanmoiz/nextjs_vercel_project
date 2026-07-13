f1d3228af4db50828c7069bb13216e66864686db
naumanmoiz <naumanmoiz@gmail.com>
2026-07-13 18:16:53 +0000

    Testing out Vercel and NextJS

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
+const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'], display: 'swap' });
+
+export { inter, lusitana }
